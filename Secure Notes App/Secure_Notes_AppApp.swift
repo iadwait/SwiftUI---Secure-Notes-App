b93afb1012795b643820e16a4baab1d11f5034a9
Adwait Barkale <adwaitbarkale@gmail.com>
2024-02-21 20:14:19 +0530

    Initial Commit

---
+++ Secure Notes App/Secure_Notes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Secure_Notes_AppApp.swift
+//  Secure Notes App
+//
+//  Created by Adwait Barkale on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Secure_Notes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
